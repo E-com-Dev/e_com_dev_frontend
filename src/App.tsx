bebdace85ab14556077ec1c562a8d3e66f937260
iamclement1 <blessedmarcel1@gmail.com>
2023-03-17 10:52:18 +0100

    update: search box

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import Home from "./pages/Home/Home";
 import { CssBaseline, Container } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./theme/theme";
+import { Home } from "./pages/Home";
+import { Navbar } from "./components/navbar";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <CssBaseline />
       <ThemeProvider theme={theme}>
         <Container maxWidth="lg">
+          <Navbar />
           <Home />
         </Container>
       </ThemeProvider>
