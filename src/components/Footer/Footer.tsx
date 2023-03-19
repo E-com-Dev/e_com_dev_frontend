8f189f7f473edb66b4315787739e89d12aa43a5e
Israel <iaboluwatife@gmail.com>
2023-03-18 21:28:52 -0400

    Successfully built the footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,64 +1,125 @@
+import styled from '@emotion/styled';
 import { Box, Container, Typography, Link } from '@mui/material';
+import Button from '@mui/material/Button/Button';
 
 
 const Footer = () => {
+
+    const MyButton = styled(Button)({
+        padding: 6,
+        margin: 6,
+        fontSize: 12,
+        fontWeight: 600,
+        backgroundColor: "#F5F5F5",
+        borderRadius: 12,
+        color: "#151515",
+        "&:hover": {
+            backgroundColor: "#cac6c6"
+        }
+    });
+
+    const MyText = styled(Typography)({
+        margin: 4,
+        fontSize: 14,
+        fontWeight: 400,
+        color: "#6A983C",
+        "&:hover": {
+            cursor: "pointer",
+            fontWeight: 600,
+        }
+    });
+
+    const MyLink = styled(Link)({
+        margin: 4,
+        fontSize: 14,
+        fontWeight: 400,
+        textDecoration: "none",
+        color: "#6A983C",
+        "&:hover": {
+            cursor: "pointer",
+            fontWeight: 600,
+        }
+    });
+
   return (
-    <div>
-        <Container sx={{display: 'flex', justifyContent: "space-between", alignItems: "flex-start", cursor: "pointer"}}>
-            <Box component="div">
-                <Typography variant="body1" fontWeight="600" fontSize="18">
+    <div >
+        <Container sx={{marginY: 4, }}>
+            <Typography variant='body1' sx={{marginY: 5, fontSize: 18, fontWeight: 600, color: "#151515"}}>
+                Shop By Brand
+            </Typography>
+            <Box sx={{marginY: 4 }}>
+                {[ "Puma", "Adidas", "Phillips", "Boohoo", "Nike", "Akira", "Aldi", "Fanyo", "Eva", "Coca cola", 
+                    "Apple", "Samsung", "Converse", "Bonobo", "Sean John", "Sure", "Seo tag", "Nivea", "Emzor" 
+                ].map((item) => (
+                    <MyButton key={item}>{item}</MyButton>
+                ))}
+            </Box>
+        </Container>
+        <Container sx={{display: 'flex', justifyContent: "space-between", alignItems: "flex-start", flexWrap: "wrap" }}>
+            <Box component="div" sx={{margin: 2, width: { xs: 100, sm: 100, md: 200} }}>
+                <Typography variant="body1" sx={{ fontWeight: 600, fontSize: 18 }} >
                     Get in touch
                 </Typography>
-                <Typography variant="body2" >
+                <MyText>
                     About Us
-                </Typography>
-                <Typography variant="body2">
+                </MyText>
+                <MyText>
                     Careers
-                </Typography>
-                <Typography variant="body2">
+                </MyText>
+                <MyText>
                     Press Releases
-                </Typography>
+                </MyText>
             </Box>
-            <Box component="div" sx={{display: 'flex', justifyContent: "space-between", flexDirection: "column",alignItems: ""}}>
-                <Typography variant="body1" fontWeight="600" fontSize="18" >
+            <Box component="div" sx={{display: 'flex', justifyContent: "space-between", flexDirection: "column", margin: 2, width: { xs:100, sm: 100, md: 200} }}>
+                <Typography variant="body1" sx={{ fontWeight: 600, fontSize: 18 }} >
                     Connections
                 </Typography>
-                <Link href="http://www.facebook.com">Facebook</Link>
-                <Link href="http://www.twitter.com">Twitter</Link>
-                <Link href="http://www.instagram.com">Instagram</Link>
-                <Link href="http://www.youtube.com">Youtube</Link>
-                <Link href="http://www.linkedin.com">LinkedIn</Link>
+                <MyLink href="http://www.facebook.com">
+                    Facebook
+                </MyLink>
+                <MyLink href="http://www.twitter.com">
+                    Twitter
+                </MyLink>
+                <MyLink href="http://www.instagram.com">
+                    Instagram
+                </MyLink>
+                <MyLink href="http://www.youtube.com">
+                    Youtube
+                </MyLink>
+                <MyLink href="http://www.linkedin.com">
+                    LinkedIn
+                </MyLink>
             </Box>
-            <Box component="div">
-                <Typography variant="body1" fontWeight="600" fontSize="18" >
+            <Box component="div" sx={{margin: 2, width: { xs: 100, sm: 100, md: 200}}}>
+                <Typography variant="body1" sx={{ fontWeight: 600, fontSize: 18 }} >
                     Earnings
                 </Typography>
-                <Typography variant="body2">
+                <MyText>
                     Advertise your product
-                </Typography>
-                <Typography variant="body2">
+                </MyText>
+                <MyText>
                     Sell on Market
-                </Typography>
+                </MyText>
             </Box>
-            <Box component="div">
-                <Typography variant="body1" fontWeight="600" fontSize="18">
+            <Box component="div" sx={{margin: 2, width: { xs: 100, sm: 100, md: 200}}}>
+                <Typography variant="body1" sx={{ fontWeight: 600, fontSize: 18 }}>
                     Account
                 </Typography>
-                <Typography variant="body2">
+                <MyText>
                     Your Account
-                </Typography>
-                <Typography variant="body2">
+                </MyText>
+                <MyText>
                     Returns Centre
-                </Typography>
-                <Typography variant="body2">
+                </MyText>
+                <MyText>
                     100 % purchase protection
-                </Typography>
-                <Typography variant="body2">
+                </MyText>
+                <MyText>
                     Chat with us
-                </Typography>
-                <Typography variant="body2">
+                </MyText>
+                <MyText>
                     Help
-                </Typography>
+                </MyText>
             </Box>
         </Container>
     </div>
