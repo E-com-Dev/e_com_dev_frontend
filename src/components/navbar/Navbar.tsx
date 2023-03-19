ab604a62271b7949a7b29dd31dd76c2457c9cad1
Kazeem Bankole <tunjibankole@gmail.com>
2023-03-19 14:49:45 +0100

    Mailicon and Notify icons removed

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -12,8 +12,8 @@ import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+
+
 import MoreIcon from '@mui/icons-material/MoreVert';
 import ShoppingBasketOutlinedIcon from '@mui/icons-material/ShoppingBasketOutlined';
 
