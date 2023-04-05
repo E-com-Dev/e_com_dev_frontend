62ef350d040842f7c6ae702c408b32254039e2ac
Jeremiah Aimiton <jeremiahaimiton@gmail.com>
2023-04-05 14:26:36 +0100

    authentication

--- src/pages/Auth/Components/AuthLayou.tsx
+++ src/pages/Auth/Components/AuthLayou.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import { IAuthLayout } from "../Interface";
 
 const AuthLayout = ({
